ebaf5684d2cd26bef5d2d873ff55ee2fd54fce91
izzat1998 <tiran.sss@mail.ru>
2019-12-25 16:16:11 +0500

    setting is configured

--- GenLib/settings.py
+++ GenLib/settings.py
@@ -123,7 +123,7 @@ EMAIL_HOST_PASSWORD = 'Izzattazzi7067600'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
@@ -136,8 +136,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "GenLib/../static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'GenLib/../media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
