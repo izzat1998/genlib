f116322a42efe7d08a1aacf33081f54bcb70afff
izzat <tiran.sss@mail.ru>
2020-07-16 23:57:53 +0500

    changed static root

--- GenLib/settings.py
+++ GenLib/settings.py
@@ -138,13 +138,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
-STATIC_URL = '/static/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = 'izzattilla706@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
